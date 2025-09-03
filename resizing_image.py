7515ce9c6253ff95847a218ae3621147f05d8ad2
Abrar Rakin <rakinabrar151@gmail.com>
2025-09-03 20:47:50 +0600

    Documentation of Resizing Image

--- resizing_image.py
+++ resizing_image.py
@@ -11,3 +11,20 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
+"""
+Step-by-Step Breakdown:
+Resizing the Image:
+
+The cv2.resize() function is used to resize the image.
+
+Setting (0, 0) for the size and using the fx=0.5 and fy=0.5 parameters scale the width and height by 50%, effectively resizing the image to half its original size.
+
+Displaying the Resized Image:
+
+The resized image is displayed in a new window using cv2.imshow(), just as the original image was displayed.
+
+Closing the Window:
+
+The window is closed using cv2.waitKey(0) and cv2.destroyAllWindows().
+
+"""
